6da5b181dccfc9dba5c2a2d6d5e097e0fc69cff3
wadad zein <wadad_zein@hotmail.com>
2021-04-27 16:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+function mondayWork(activity="go to the office"){
+  return (`This Monday, I will ${activity}.`);
+}
+
+function saturdayFun(activity="roller-skate"){
+  return (`This Saturday, I want to ${activity}!`);
+}
+ saturdayFun("bathe my dog");
+
+// function wrapAdjective("*")("a dedicated programmer"){
+//   function("*")(parameter="special"){
+// return (`You are ${parameter`})
+// }
+
+function wrapAdjective(flairString="*") {
+  return function(adj="special") {
+    return `You are ${flairString}${adj}${flairString}!`;
+  } ;
+}
+
+
+const Calculator = {
+  add: (function(a, b){ return a + b}),
+  subtract: (function(a, b){ return a - b}),
+  multiply: (function(a, b){ return a * b}),
+  divide: (function(a, b){ return a / b})
+} ;
+
+function actionApplyer(starter, fnArray) {
+  let int = starter;
+  for (const fn of fnArray) {
+   int = fn(int)}
+  return int;
+} 
+
